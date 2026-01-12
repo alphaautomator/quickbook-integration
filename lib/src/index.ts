2e6ae61da4442d404f9991788a6d911b495b92c2
alpharaghav301 <119299019+alpharaghav301@users.noreply.github.com>
2026-01-13 02:52:55 +0530

    exporting the client.

--- lib/src/index.ts
+++ lib/src/index.ts
@@ -1,14 +1,21 @@
 // Shared library main exports
 // This file will export all shared modules
 
+// QuickBooks API Client
+export { QuickBooks } from './quickbooks/client';
+export { 
+  getAuthorizationUrl, 
+  exchangeCodeForTokens, 
+  refreshAccessToken, 
+  revokeToken 
+} from './quickbooks/oauth';
+export type { OAuthTokenResponse } from './quickbooks/oauth';
+
 // Database
 export { getDb, initializeDatabase, closeDb, transaction } from './database/connection';
 export { tokenRepository } from './database/repositories/token.repository';
 export type { TokenData } from './database/repositories/token.repository';
 
 // Config and utilities
-// export { config, validateConfig } from './config';
+export { config, validateConfig } from './config';
 export { logger } from './utils/logger';
-
-// QuickBooks client will be exported after it's created
-// export { QuickBooks } from './quickbooks/client';
